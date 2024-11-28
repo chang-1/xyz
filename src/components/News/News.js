b9898a399506e4b40c6062d09e0a01c8a68b2773
chang-1 <53927233+chang-1@users.noreply.github.com>
2024-11-28 17:39:07 -0500

    Update News.js

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,8 +4,11 @@ export default function News(props){
     return(
         <section className="news_wrapper" id="news">
             <h2 className='news_heading'>Education</h2>
-            <p className='research_text'>  I'm interested in computer vision, deep learning, generative AI, and image processing. Most of my research is about inferring the physical world (shape, motion, color, light, etc) from images, usually with radiance fields. Some papers are highlighted.</p>
-
+            <p className='research_text'>
+            I earned my <strong>Master of Science in Computer Science</strong> from <a href="https://www.brown.edu/" target="_blank" rel="noopener noreferrer">Brown University</a>, focusing on visual computing and machine learning, and a <strong>Bachelor of Science in Computer Engineering</strong> from <a href="https://www.itam.mx/" target="_blank" rel="noopener noreferrer">Instituto Tecnológico Autónomo de México</a>. During my undergraduate studies, I discovered my passion for computer vision and collaborated with <a href="https://scholar.google.com/citations?user=Me-kqtIAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Dr. Edgar Román-Rangel</a> to reconstruct artistic images from highly compressed representations.
+            </p>
         </section>
     )
 }
+
+
