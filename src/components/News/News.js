38fea0c41b21c8496155e0c3e839117ccdbd89ec
chang-1 <53927233+chang-1@users.noreply.github.com>
2024-11-28 17:29:25 -0500

    Update News.js

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,22 +3,9 @@ import '../News/News.css'
 export default function News(props){
     return(
         <section className="news_wrapper" id="news">
-            <h2 className='news_heading'>News</h2>
-            <div>
-                <a href="/"><h5 className='news_title'>EVER: Exact Volumetric Ellipsoid Rendering for Real-time View Synthesis</h5></a>
-                <p className='news_date'>November 29, 2024</p>
-                <hr/>
-            </div>
-            <div>
-                <a href="/"><h5 className='news_title'>EVER: Exact Volumetric Ellipsoid Rendering for Real-time View Synthesis</h5></a>
-                <p className='news_date'>November 29, 2024</p>
-                <hr/>
-            </div>
-            <div>
-                <a href="/"><h5 className='news_title'>EVER: Exact Volumetric Ellipsoid Rendering for Real-time View Synthesis</h5></a>
-                <p className='news_date'>November 29, 2024</p>
-                <hr/>
-            </div>
+            <h2 className='news_heading'>Education</h2>
+            <p className='research_text'>  I'm interested in computer vision, deep learning, generative AI, and image processing. Most of my research is about inferring the physical world (shape, motion, color, light, etc) from images, usually with radiance fields. Some papers are highlighted.</p>
+
         </section>
     )
 }
