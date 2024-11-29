47c6e064485c14be3a652632b0e9add576eff220
chang-1 <53927233+chang-1@users.noreply.github.com>
2024-11-28 20:44:45 -0500

    Update News.js

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -6,7 +6,7 @@ export default function News(props){
             <h2 className='news_heading'>Education</h2>
             <p className='research_text'>
                 During my undergraduate studies, I discovered my passion for computer vision under the mentorship of 
-                <a href="https://scholar.google.com/citations?user=Me-kqtIAAAAJ&hl=en" target="_blank" rel="noopener noreferrer"> Dr. Edgar Román-Rangel</a>, collaborating with him to reconstruct artistic images from highly compressed representations.
+                <a href="https://scholar.google.com/citations?user=Me-kqtIAAAAJ&hl=en" target="_blank" rel="noopener noreferrer"> Dr. Edgar Román-Rangel</a>, with whom I collaborated to reconstruct artistic images from highly compressed representations.
             </p>
         </section>
     )
