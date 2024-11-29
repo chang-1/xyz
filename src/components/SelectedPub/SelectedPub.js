b1523576f020f81bf42bcb6cc078c371203c0c93
chang-1 <53927233+chang-1@users.noreply.github.com>
2024-11-28 20:16:35 -0500

    Update SelectedPub.js

--- src/components/SelectedPub/SelectedPub.js
+++ src/components/SelectedPub/SelectedPub.js
@@ -2,13 +2,32 @@ import '../SelectedPub/SelectedPub.css'
 import Image from '../../assets/images/zoriginal1.gif'
 import Bobbly from '../../assets/images/bobbly.gif'
 import CoolImage from '../../assets/images/image.jpg'
+import PCART from '../../assets/images/pcart.png'
 
 export default function SelectedPub(props){
     const isMobile = props.width < 800;
 
     return(
         <section className={`research ${ isMobile?'col-12':'col-8'}`} id="publication">
-            <h2 className='research_title'> Selected Projects</h2>
+            <h2 className='research_title'> Research & Projects</h2>
+
+           {/* Research Item 1*/}
+        <div className="research_item--wrapper">
+            <div className={`${isMobile?'col-12':'col-4'}`}>
+                <a href="https://github.com/jorgechang/Artistic-Image-Recovering-from-Principal-Components/blob/main/Artistic_Image_Recovering_from_Principal_Components.pdf" target="_blank" rel="noopener noreferrer">
+                    <h5 className="research_title">
+                    Artistic Image Recovering from Principal Components
+                    </h5>
+                </a>
+                <p className="research_body">Evaluated the capacity of convolutional neural networks to reconstruct artistic images from highly compressed latent representations (1% of the original feature
+                    space) and presented results at the <a href="http://lacsc2020.itam.mx/index.php/lacsc-2020/" target="_blank" rel="noopener noreferrer">V Latin American Conference on Statistical Computing</a> .</p>
+
+            </div>
+                        
+            <div className={`research_image--container ${isMobile?'col-12':'col-3'}`}>
+                <img className="research_image" src={PCART} alt="#"/>
+            </div>
+        </div>
             
 
             {/* Research Item 1*/}
