1035c95f0cb353d66fd0d53b55f9c92151e437c1
chang-1 <53927233+chang-1@users.noreply.github.com>
2024-11-28 19:23:59 -0500

    Update Research.js

--- src/components/Research/Research.js
+++ src/components/Research/Research.js
@@ -1,5 +1,5 @@
 import '../Research/Research.css'
-import Image from '../../assets/images/image.jpg'
+import Image from '../../assets/images/pcart.png'
 
 export default function Research(props){
     const isMobile = props.width < 800;
@@ -8,6 +8,30 @@ export default function Research(props){
         <section className={`research ${ isMobile?'col-12':'col-8'}`} id="research">
             <h2 className='research_title'> Research Interest</h2>
             <p className='research_text'>  I'm interested in computer vision, neuroscience, deep learning, and generative AI.</p>
+
+
+            {/* Research Item 1*/}
+        <div className="research_item--wrapper">
+            <div className={`${isMobile?'col-12':'col-4'}`}>
+                <a href="https://github.com/jorgechang/Artistic-Image-Recovering-from-Principal-Components/blob/main/Artistic_Image_Recovering_from_Principal_Components.pdf" target="_blank" rel="noopener noreferrer">
+                    <h5 className="research_title">
+                    Artistic Image Recovering from Principal Components
+                    </h5>
+                </a>
+                <p className="research_body">Evaluated the capacity of convolutional neural networks to reconstruct artistic images from highly compressed latent representations (1% of the original feature
+                    space) and presented results at the <a href="http://lacsc2020.itam.mx/index.php/lacsc-2020/" target="_blank" rel="noopener noreferrer">V Latin American Conference on Statistical Computing</a> .</p>
+
+            </div>
+            <div className={`research_image--container ${isMobile?'col-12':'col-3'}`}>
+                <img className="research_image" src={Image} alt="#"/>
+            </div>
+        </div>
+
+        
         </section>
+        
+
+
+
     )
 }
